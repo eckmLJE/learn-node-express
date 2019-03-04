34a30777e5230ea22361180cec5ef456fa36250c
Lucas Eckman <lucas.eckman@gmail.com>
2019-03-04 09:50:32 -0500

    part 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import models from "./models";
+import routes from "./routes";
 
-console.log("Hello Node.js project.");
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}`)
+);
