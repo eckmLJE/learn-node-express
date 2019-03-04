6452de5ecc34ee6b62b5916879682d7eef42e41b
Lucas Eckman <lucas.eckman@gmail.com>
2019-03-04 11:04:34 -0500

    routes working

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import "dotenv/config";
-import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import routes from "./routes";
+import express from "express";
+
 import models, { sequelize } from "./models";
+import routes from "./routes";
 
 const app = express();
 
+// Application-Level Middleware
+
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin("rwieruch")
+  };
+  next();
+});
+
+// Routes
 
 app.use("/session", routes.session);
 app.use("/users", routes.user);
 app.use("/messages", routes.message);
+
+// Start
 
 const eraseDatabaseOnSync = true;
 
