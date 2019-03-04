917de3e2cdbc1ce618404ea856695c1451fb6f55
Lucas Eckman <lucas.eckman@gmail.com>
2019-03-04 10:50:14 -0500

    postgres connected

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1]
-  };
-  next();
-});
-
 app.use("/session", routes.session);
 app.use("/users", routes.user);
 app.use("/messages", routes.message);
